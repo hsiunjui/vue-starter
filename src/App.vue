4555d9fcf059c62eee350cdc04ac7798c3e34d9c
Jeoi <Hung.Jeoi@outlook.com>
2024-02-02 06:24:02 +0800

    vite+vue2+cubeui

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
-<script setup lang="ts">
-// import { ref } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue'
-const a = 1
-const show = ref(false)
-const { x, y } = useMouse()
-console.log(a)
+<template>
+  <div id="app" class="bg-black">
+    <nav>
+      <cube-button>你好</cube-button>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
 
-const onShow = () => {
-  show.value = !show.value
+<script>
+// let a = 1
+export default {
+  data() {
+    return {}
+    // init: {
+    //   a: 1
+    // }
+  }
 }
 </script>
 
-<template>
-  <div class="p-1 bg-sun1">
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link><br />
-    <router-view />
-    <div class="test animate__animated animate__bounce" v-if="show">this is text</div>
-    <van-button type="primary" text="212" @click="onShow" />
-    <div class="text-red-700">pos: {{ x }}, {{ y }}</div>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped></style>
+<style scoped lang="scss">
+#app {
+  // background: red;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        sun: '#ffb',
-        sun1: '#f12'
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
+
