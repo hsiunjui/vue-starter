8b545fda623b6cbb8fd49b4db93cc7fb75af7787
Jeoi <Hung.Jeoi@outlook.com>
2024-02-02 06:26:41 +0800

    nuxt+vue3+vant+elementplus

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-export default {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*'],
-      unitPrecision: 5
-    }
-  }
+  },
 }
